c7ac4d62a6b43d04fd944f294b8db89daa034390
Zachary Petti <zachmpetti@gmail.com>
2023-08-16 10:17:38 -0400

    Second star day 3

--- 3/day_three.hs
+++ 3/day_three.hs
@@ -10,8 +10,8 @@ unwrap e a = case a of
 getPriority :: Char -> Integer
 getPriority item = snd . (unwrap "Priority") $ find (\(char, _) -> char == item) (zip (['a'..'z'] ++ ['A'..'Z']) ([1..]))
 
-toCompartments :: String -> (String, String)
-toCompartments s = splitAt ((length s) `div` 2) s
+-- toCompartments :: String -> (String, String)
+-- toCompartments s = splitAt ((length s) `div` 2) s
 
 findBoth :: ([Char] , [Char]) -> Char
 findBoth ((head:tail), search) = 
@@ -19,10 +19,35 @@ findBoth ((head:tail), search) =
   Just a -> a 
   Nothing -> findBoth (tail, search)
 
+allChar :: [Maybe Char] -> Maybe Char 
+allChar [char] = char
+allChar (head:tail) =
+  case head of 
+  Nothing -> Nothing
+  Just char -> case (allChar tail) of 
+    Just sec_char -> if sec_char == char 
+      then Just char 
+      else Nothing
+    Nothing -> Nothing
+
+
+
+findAll :: [String] -> Char 
+findAll ([]:_) = error "Findall failed to find all inputs"
+findAll ((head:tail):searches) = 
+  case allChar $ map (find (== head)) searches of 
+  Nothing -> findAll (tail:searches)
+  Just a -> a
+
+
+-- [[S,S,S],[S,S,S]]
+chunk :: [String] -> [[String]]
+chunk [] = []
+chunk a = (chunk (drop 3 a)) ++ [(take 3 a)]
 
 
 totalSum :: String -> Integer
-totalSum = sum . map (getPriority . findBoth . toCompartments) . lines 
+totalSum = sum . map (getPriority . findAll) .  chunk . lines 
 
 main :: IO ()
 main = do
