1bec9399c01704ffe937c91e58d045603d2b0d2b
Zachary Petti <zachmpetti@gmail.com>
2023-08-17 16:23:20 -0400

    Make Utils module

---
+++ util/Utils.hs
@@ -0,0 +1,13 @@
+module Utils where
+
+removeCarriage = filter (/= '\r')
+
+split :: Char -> String -> [String]
+split c = wordsWhen (== c)
+
+wordsWhen :: (Char -> Bool) -> String -> [String]
+wordsWhen p s = case dropWhile p s of
+  "" -> []
+  s' -> w : wordsWhen p s''
+    where
+      (w, s'') = break p s'
