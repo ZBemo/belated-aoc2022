9e7aed7a11f7f888f1d38667cd5d09c3f7b7e536
Zachary Petti <zachmpetti@gmail.com>
2023-08-19 15:07:48 -0400

    Make split strict

--- util/Utils.hs
+++ util/Utils.hs
@@ -8,6 +8,6 @@ split c = wordsWhen (== c)
 wordsWhen :: (Char -> Bool) -> String -> [String]
 wordsWhen p s = case dropWhile p s of
   "" -> []
-  s' -> w : wordsWhen p s''
+  s' -> (w:) $! wordsWhen p s''
     where
       (w, s'') = break p s'
